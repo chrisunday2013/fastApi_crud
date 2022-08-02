30b922a6ccaf11d1e38cad82d491e18a2d0b9904
Dada Oluwashina <dada.oluwashina@longbridgetech.com>
2022-08-02 22:33:39 +0100

    first commit

---
+++ student.py
@@ -0,0 +1,86 @@
+from fastapi import FastAPI, Path
+from typing import Optional
+from pydantic import BaseModel
+
+app = FastAPI()
+
+students = {1: {"name": "john", "sex": "male", "age": 20, "year": "year 5" } }
+
+class Student(BaseModel):
+    name: str
+    sex: str
+    age: int 
+    year: str 
+
+
+class UpdateStudent(BaseModel):
+    name: Optional[str] = None 
+    sex: Optional[str] = None
+    age: Optional[int] = None 
+    year: Optional[str] = None    
+
+@app.get("/")
+def home():
+    return {"name": "Home Page"}
+
+
+@app.get("/get-student/{student_id}")
+def get_student(student_id: int = Path(None, description="The ID of the student you want to view", gt=0, lt=10)):
+    return students[student_id] 
+
+
+
+@app.get("/get-by-name")
+def get_student( name: Optional[str] = None):
+    for student_id in students:
+          if students[student_id]["name"] == name:
+              return students[student_id]
+    return {"Data": "Not found"}    
+
+
+
+@app.get("/get-by-name/{student_id}")
+def get_student(student_id: int, name: Optional[str] = None):
+    for student_id in students:
+          if students[student_id]["name"] == name:
+              return students[student_id]
+    return {"Data": "Not found"}    
+
+
+
+@app.post("/create-student/{student_id}")
+def create_student(student_id : int, student: Student):
+    if student_id in students:
+        return {"Error": "Student already exist"}
+
+    students[student_id] = student
+    return students[student_id]    
+
+
+@app.put("/update-student/{student_id}")
+def update_student(student_id: int, student: UpdateStudent):
+    if student_id not in students:
+         return {"Error": "Student does not exit"}
+
+    if student.name != None:
+        students[student_id].name = student.name
+
+
+    if student.age !=None:
+        students[student_id].age = student.age
+
+    if student.year != None:
+        students[student_id].year = student.year    
+
+    return students[student_id]     
+
+
+@app.delete("/delete-student/{student_id}")
+def delete_student(student_id: int):
+    if student_id not in students:
+        return {"Error": "Student does not exist"}
+
+
+    del students[student_id]
+    return {"Message": "Student deleted successfully"}                
+
